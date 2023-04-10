fdc13739d9cadb0709b1d8a3a2d85befecb08572
Galih Saputra <galih.saputra@asyx.com>
2023-04-10 20:33:18 +0700

    Initial commit
    - initiate vite + react
    - add tailwindcss
    - add eslint + prettier for linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['prettier', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
